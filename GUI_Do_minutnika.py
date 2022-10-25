d9daa44e22da0b0e082b086181482a2bbd067724
NEDRI <100300622+NEDRI@users.noreply.github.com>
2022-10-25 21:32:54 +0200

    Update GUI_Do_minutnika.py

--- GUI_Do_minutnika.py
+++ GUI_Do_minutnika.py
@@ -80,7 +80,7 @@ czydzialal = False
 aplikacja.columnconfigure(0, minsize=225)
 aplikacja.rowconfigure([0, 1], minsize=100)
 
-bg = PhotoImage(file = "C:\\Users\\andrz\\Downloads\\bgi.png")
+bg = PhotoImage(file = "plik")
 
 
 label1 = Label( aplikacja, image = bg)
