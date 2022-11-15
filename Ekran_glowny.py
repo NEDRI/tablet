f60aa7236aab11331818f421b4b507a1a3a289b8
NEDRI <100300622+NEDRI@users.noreply.github.com>
2022-11-15 11:29:08 +0100

    Update Ekran_glowny.py

--- Ekran_glowny.py
+++ Ekran_glowny.py
@@ -1,17 +1,14 @@
 import time
-import sys
-from tkinter import Tk, Label, Button
-from tkinter import ttk, Button
+from tkinter import Label, Button
 import tkinter as tk
 import sqlite3
 from tkinter import *
-import platform
 from time import strftime 
 
-# funkcja do tła
+# funkcja do koloru tła
 def _from_rgb(rgb):
     return "#%02x%02x%02x" % rgb
-
+# wyswietlanie czasu
 def time(): 
 
     string = strftime('%H:%M:%S') 
@@ -20,37 +17,26 @@ def time():
 
     gtext_label.after(1000, time)
 
-'''
-def zegar():
-    while True:
-        from datetime import datetime
-        now = datetime.now()
-        gtext_label = Label(aplikacja, font= 36, text= "%s:%s:%s" % (now.hour,now.minute,now.second))
-        gtext_label.place(x= 370, y=30)
-        sys.stdout.flush()
-        print("\r"),
-        time.sleep(1)
-'''
-
+# ekran 
 aplikacja = tk.Tk()
 aplikacja.title("SFA")
 aplikacja.geometry("800x480")
 aplikacja.configure(bg=_from_rgb((240, 248, 255)))
-
+# obraz w tle
 bg= PhotoImage(file="/home/ai/Downloads/tlomenu.png")
-
+# tlo wyswietlanie 
 label1= Label(aplikacja, image = bg)
 label1.place(x=0, y=0)
-
+#zegar 
 gtext_label = Label(aplikacja, font= 36)
 gtext_label.place(x= 370, y=30)
-
+#przycisk  do minutnika
 Przycisk_minutnik=Button(aplikacja, text="Minutnik", bg="black", fg="white", font=36, width=7, height=2)
 Przycisk_minutnik.place(x=300, y=200)
-
+#przycisk do produkty
 Przycisk_baza=Button(aplikacja, text="produkty", bg="black", fg="white", font=36, width=7, height=2)
 Przycisk_baza.place(x=400, y=200)
-
+#wyswietlanie na ekran 
 time()
 aplikacja.mainloop()
 
