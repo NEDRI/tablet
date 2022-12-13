5d40bf1d802af4607e82dbc725eca49fd5368d8a
Hubert105 <99913416+Hubert105@users.noreply.github.com>
2022-12-13 10:02:53 +0100

    Update Minutnik.py

--- Minutnik.py
+++ Minutnik.py
@@ -6,6 +6,11 @@ import time
 import customtkinter
 
 #funkcja 
+# funkcja do koloru tła
+def _from_rgb(rgb):
+    return "#%02x%02x%02x" % rgb
+
+
 def Minutnik(czas):
 	
 	while czas:
@@ -76,7 +81,8 @@ aplikacja.geometry("800x480")
 
 #zmiene
 liczba= 0
-szerokosc=10 #szerokosc przycisku
+szerokosc=80 #szerokosc przycisku
+wys=65
 lewo=5
 srodek=7
 prawo=9
@@ -88,56 +94,53 @@ aplikacja.columnconfigure(0, minsize=225)
 aplikacja.rowconfigure([0, 1], minsize=100)
 #tlo
 
-bg = PhotoImage(file = "/home/ai/gitfl/gitfl/tablet/tlomenu.png")
 
-label1 = Label( aplikacja, image = bg)
-label1.place(x = 0, y = 0)
 
 #menu
 gtext_label = customtkinter.CTkLabel(aplikacja,text="Wybierz czas:")
 gtext_label.place(x = 350, y = 50)
 
 #tu ma byc czas wyswietlany
-text_label = Label(aplikacja, font=80, bg="black", fg="white",  text=czas)
+text_label = Label(aplikacja, font=80, bg=_from_rgb((555, 555, 555)), fg="white",  text=czas)
 text_label.place(x = 380, y = 100)
 
-Przycisk1= customtkinter.CTkButton(aplikacja, text="1",width=szerokosc, height=3, command=lambda: ustawczas(1))
-Przycisk1.place(x = 210, y = 200)
+Przycisk1= customtkinter.CTkButton(aplikacja, text="1",width=szerokosc, height=wys, command=lambda: ustawczas(1))
+Przycisk1.place(x = 255, y = 200)
 
-Przycisk2= customtkinter.CTkButton(aplikacja, text="2",width=szerokosc, height=3, command=lambda: ustawczas(2))
-Przycisk2.place(x = 335, y = 200)
+Przycisk2= customtkinter.CTkButton(aplikacja, text="2",width=szerokosc, height=wys, command=lambda: ustawczas(2))
+Przycisk2.place(x = 350, y = 200)
 
-Przycisk3= customtkinter.CTkButton(aplikacja, text="3",width=szerokosc, height=3, command=lambda: ustawczas(3))
-Przycisk3.place(x = 460, y = 200)
+Przycisk3= customtkinter.CTkButton(aplikacja, text="3",width=szerokosc, height=wys, command=lambda: ustawczas(3))
+Przycisk3.place(x = 445, y = 200)
 
-Przycisk4= customtkinter.CTkButton(aplikacja, text="4",width=szerokosc, height=3, command=lambda: ustawczas(4))
-Przycisk4.place(x = 210, y = 270)
+Przycisk4= customtkinter.CTkButton(aplikacja, text="4",width=szerokosc, height=wys, command=lambda: ustawczas(4))
+Przycisk4.place(x = 255, y = 270)
 
-Przycisk5= customtkinter.CTkButton(aplikacja, text="5",width=szerokosc, height=3, command=lambda: ustawczas(5))
-Przycisk5.place(x = 335, y = 270)
+Przycisk5= customtkinter.CTkButton(aplikacja, text="5",width=szerokosc, height=wys, command=lambda: ustawczas(5))
+Przycisk5.place(x = 350, y = 270)
 
-Przycisk6= customtkinter.CTkButton(aplikacja, text="6",width=szerokosc, height=3, command=lambda: ustawczas(6))
-Przycisk6.place(x = 460, y = 270)
+Przycisk6= customtkinter.CTkButton(aplikacja, text="6",width=szerokosc, height=wys, command=lambda: ustawczas(6))
+Przycisk6.place(x = 445, y = 270)
 
-Przycisk7= customtkinter.CTkButton(aplikacja, text="7",width=szerokosc, height=3, command=lambda: ustawczas(7))
-Przycisk7.place(x = 210, y = 340)
+Przycisk7= customtkinter.CTkButton(aplikacja, text="7",width=szerokosc, height=wys, command=lambda: ustawczas(7))
+Przycisk7.place(x = 255, y = 340)
 
-Przycisk8= customtkinter.CTkButton(aplikacja, text="8",width=szerokosc, height=3, command=lambda: ustawczas(8))
-Przycisk8.place(x = 335, y = 340)
+Przycisk8= customtkinter.CTkButton(aplikacja, text="8",width=szerokosc, height=wys, command=lambda: ustawczas(8))
+Przycisk8.place(x = 350, y = 340)
 
-Przycisk9= customtkinter.CTkButton(aplikacja, text="9",width=szerokosc, height=3, command=lambda: ustawczas(9))
-Przycisk9.place(x = 460, y = 340)
+Przycisk9= customtkinter.CTkButton(aplikacja, text="9",width=szerokosc, height=wys, command=lambda: ustawczas(9))
+Przycisk9.place(x = 445, y = 340)
 
-Przycisk00= customtkinter.CTkButton(aplikacja, text="00s",width=szerokosc, height=3, command=zerozero)
-Przycisk00.place(x = 210, y = 410)
+Przycisk00= customtkinter.CTkButton(aplikacja, text="00s",width=szerokosc, height=wys, command=zerozero)
+Przycisk00.place(x = 255, y = 410)
 
-Przycisk0= customtkinter.CTkButton(aplikacja, text="0",width=szerokosc, height=3, command=zero)
-Przycisk0.place(x = 335, y = 410)
+Przycisk0= customtkinter.CTkButton(aplikacja, text="0",width=szerokosc, height=wys, command=zero)
+Przycisk0.place(x = 350, y = 410)
 
-PrzyciskBack= customtkinter.CTkButton(aplikacja, text="del",width=szerokosc, height=3, command=liczbadel)
-PrzyciskBack.place(x = 460, y = 410)
+PrzyciskBack= customtkinter.CTkButton(aplikacja, text="del",width=szerokosc, height=wys, command=liczbadel)
+PrzyciskBack.place(x = 445, y = 410)
 
-PMinutnik= customtkinter.CTkButton(aplikacja, text="start",width=szerokosc, height=3, command=Minutnik2)
-PMinutnik.place(x = 585, y = 410)
+PMinutnik= customtkinter.CTkButton(aplikacja, text="start",width=szerokosc, height=wys, command=Minutnik2)
+PMinutnik.place(x = 540, y = 410)
 
 aplikacja.mainloop()
