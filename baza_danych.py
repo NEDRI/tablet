1a66ced253d5341d43880ef5adccde124c50de50
NEDRI <jedrek.iwaszkiewcz@gmail.com>
2022-12-13 11:17:10 +0100

    w

--- baza_danych.py
+++ baza_danych.py
@@ -70,7 +70,7 @@ aplikacja.title("SFA")
 aplikacja.geometry("800x480")
 aplikacja.configure(bg=_from_rgb((240, 248, 255)))
 
-bazasfa="E:\\projekt_SFA\\kod\\bazasfa"
+bazasfa="/home/ai/gitfl/gitfl/tablet/bazasfa"
 drzewo = ttk.Treeview(aplikacja, columns=("kolumna1", "kolumna2"), show="headings")
 drzewo.heading("#1", text="Nazwa produktu")
 drzewo.heading("#2", text="Ilość produktu")
