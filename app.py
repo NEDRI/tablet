aa1c453985406dbedb97c2cf17fc654da2b05f9f
NEDRI <jedrek.iwaszkiewicz@gmail.com>
2023-07-06 19:45:57 +0200

    a

--- app.py
+++ app.py
@@ -398,7 +398,7 @@ def app3_pokaz(ramka):
 if __name__ == "__main__":
     #tworze ekran
     customtkinter.set_appearance_mode("dark")
-    customtkinter.set_default_color_theme("blue")
+    customtkinter.set_default_color_theme("green")
     root = customtkinter.CTk()
     root.title("SFA")
     root.geometry("800x480")
